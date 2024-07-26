20c79f84be765c918f22e197520f581f21f959e5
Daniel Sousa <sousadanielsb@gmail.com>
2024-07-25 21:36:26 -0300

    Ciclo 9

--- Test/test_mostrar.py
+++ Test/test_mostrar.py
@@ -18,3 +18,13 @@ def test_mostrar_um_Evento(agenda, capsys):
     out, _ = capsys.readouterr()
     assert len(agenda.eventos) == 1
     assert out.strip() == "Final dos 100m: 2024-07-24 10:00 a 2024-07-24 11:00"
+
+def test_mostrar_dois_ou_mais_Eventos(agenda, capsys):
+    agenda.add_Eventos("Final dos 100m", "2024-07-24 10:00", "2024-07-24 11:00")
+    out, _ = capsys.readouterr()
+    agenda.add_Eventos("Final dos 200m", "2024-07-24 11:30", "2024-07-24 12:30")
+    out, _ = capsys.readouterr()
+    assert len(agenda.eventos) == 2
+    agenda.get_Eventos()
+    out, _ = capsys.readouterr()
+    assert out.strip() == "Final dos 100m: 2024-07-24 10:00 a 2024-07-24 11:00\nFinal dos 200m: 2024-07-24 11:30 a 2024-07-24 12:30"
